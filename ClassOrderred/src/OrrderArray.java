f50baa638f4357f185bf0336b435db81767c9237
Andrey1582 <anostanin.2016@gmail.com>
2024-11-01 14:47:41 +0500

    OrrderArray.java

--- ClassOrderred/src/OrrderArray.java
+++ ClassOrderred/src/OrrderArray.java
@@ -26,6 +26,34 @@ public class OrrderArray extends Array{
         }
     }
 
+    public boolean delete(long searchValue) {
+        int lower = 0;
+        int upper = n - 1;
+        int curElem = -1;
+
+        while (lower <= upper) {
+            int mid = (lower + upper) / 2;
+            if (array[mid] == searchValue) {
+                curElem = mid;
+                break;
+            } else if (array[mid] < searchValue) {
+                lower = mid + 1;
+            } else {
+                upper = mid - 1;
+            }
+        }
+
+        if (curElem == -1) {
+            return false;
+        }
+
+        for (int i = curElem; i < n - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        n--;
+        return true;
+    }
+    
     public boolean insert(long value) {
         if (this.contains(value)) {
             return false;
@@ -46,7 +74,7 @@ public class OrrderArray extends Array{
         n++;
         return true;
     }
-
+    
     public int getMaxElem()
     {
         return (int) array[array.length - 1];
